88649ca3c509c6fd85261b8a50349209a7dc6184
MagmaWaves <luluduwors@gmail.com>
2025-03-25 23:37:06 -0700

    Update SwerveBase.java
    setServoAngles
    setMotorPowers

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SwerveBase.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SwerveBase.java
@@ -3,7 +3,7 @@ package org.firstinspires.ftc.teamcode;
 public class SwerveBase extends Robot.HardwareDevices {
     private Robot robot;
 
-    //0 is global, and the rest are in order
+    //5 is global, and the rest are in order
     public double[] swerveServoAngleOffset = {0, 0, 0, 0, 0};
 
     public SwerveBase(Robot robot) {
@@ -11,12 +11,24 @@ public class SwerveBase extends Robot.HardwareDevices {
     }
 
     //Gets the positions in degrees of the swerve module servos
-    public double[] getSwerveServoAngles(){
+    public double[] getSwerveServoAngles() {
         double[] positions = new double[4];
         for (int i = 0; i < positions.length; i++) {
-            positions[i] = swerveAnalogs[i].getVoltage() / 3.3 * 360 + swerveServoAngleOffset[0] + swerveServoAngleOffset[i + 1];
+            positions[i] = swerveAnalogs[i].getVoltage() / 3.3 * 360 + swerveServoAngleOffset[5] + swerveServoAngleOffset[i + 1];
         }
 
         return positions;
+    }
+
+    public void setServoAngles(double[] servoPositions) {
+        for (int i = 0; i < servoPositions.length; i++) {
+            swerveServos[i].setPower(servoPositions[i]);
+        }
+    }
+
+    public void setMotorPowers(double[] motorPowers) {
+        for (int i = 0; i < motorPowers.length; i++) {
+            swerveMotors[i].setPower(motorPowers[i]);
+        }
     }
 }
