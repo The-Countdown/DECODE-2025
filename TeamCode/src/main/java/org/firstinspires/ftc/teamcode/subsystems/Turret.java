ed3e8301c21986f437c397d46906e75312b5dbd7
MagmaWaves <luluduwors@gmail.com>
2025-04-17 20:51:17 -0700

    i forgot

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Turret.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Turret.java
@@ -17,9 +17,13 @@ public class Turret extends RobotContainer.HardwareDevices {
     }
 
     // the input is 0-1 not degrees, unless you convert it
+<<<<<<< Updated upstream
     public void setArcAngle(double degrees) {
         RobotContainer.HardwareDevices.turretArcServo.setPosition(degrees); }
 
+=======
+    public void setArcAngle(double angle) { RobotManager.HardwareDevices.turretArcServo.setPosition(angle); }
+>>>>>>> Stashed changes
     public class Flywheel {
         public void setPower(double power) {
             robotContainer.turretFlywheel.setPower(power);
