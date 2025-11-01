d6418147560ef06661e80d852f450cc32b1ee447
Robot <colepearson27@proton.me>
2025-11-01 08:33:24 -0700

    fixed auto transfer

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/main/Constants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/main/Constants.java
@@ -291,7 +291,7 @@ public class Constants {
     @Config
     public static class Spindexer {
         public static int NUM_SLOTS = 3;
-        public static double ANGLE_OFFSET = 25;
+        public static double ANGLE_OFFSET = 35;
         public static double[] TRANSFER_SLOT_ANGLES = {179, 60, -60};
         public static double [] INTAKE_SLOT_ANGLES = {0, -120, 120};
         public static double KP = 0.012;
