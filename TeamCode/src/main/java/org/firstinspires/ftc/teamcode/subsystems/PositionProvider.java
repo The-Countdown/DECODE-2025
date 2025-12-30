f29343dfbd90439df286b07faa306cc44a682d6f
elliot <elliotwfs@gmail.com>
2025-12-30 12:37:24 -0800

    The LimeLight with heading is alright but we need to find a way to rotate the odPose x and y by the vision offset heading

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/PositionProvider.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/PositionProvider.java
@@ -1,6 +1,7 @@
 package org.firstinspires.ftc.teamcode.subsystems;
 
 import com.qualcomm.hardware.gobilda.GoBildaPinpointDriver;
+import com.qualcomm.robotcore.util.AndroidSerialNumberNotFoundException;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
 import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
@@ -8,6 +9,7 @@ import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 import org.firstinspires.ftc.robotcore.external.navigation.Pose2D;
 import org.firstinspires.ftc.teamcode.main.RobotContainer;
 import org.firstinspires.ftc.teamcode.main.Status;
+import org.firstinspires.ftc.teamcode.util.HelperFunctions;
 import org.firstinspires.ftc.teamcode.util.LimeLightInfo;
 
 import java.util.ArrayList;
@@ -21,6 +23,7 @@ public class PositionProvider {
     private ElapsedTime visionTimer = new ElapsedTime();
     private ArrayList<Pose2D> visionPoseList;
     private Pose2D lastODPose;
+    private Pose2D startODPose;
 
     public PositionProvider(RobotContainer robotContainer, LimelightLogic limelightLogic, GoBildaPinpointDriver pinpoint) {
         this.robotContainer = robotContainer;
@@ -34,7 +37,7 @@ public class PositionProvider {
         Pose2D odPose = pinpoint.getPosition();
         double rx = (odPose.getX(DistanceUnit.CM) + visionOffsetPose.getX(DistanceUnit.CM)) * 0.9914129;
         double ry = (odPose.getY(DistanceUnit.CM) + visionOffsetPose.getY(DistanceUnit.CM)) * 0.998610856289;
-        double rHeading = odPose.getHeading(AngleUnit.DEGREES);
+        double rHeading = (odPose.getHeading(AngleUnit.DEGREES) + visionOffsetPose.getHeading(AngleUnit.DEGREES));
         return new Pose2D(DistanceUnit.CM, rx, ry, AngleUnit.DEGREES, rHeading);
     }
 
@@ -50,6 +53,7 @@ public class PositionProvider {
     public void update(boolean LimeLight) {
         if (LimeLight) {
             Pose2D odPose = RobotContainer.HardwareDevices.pinpoint.getPosition();
+
             Pose2D visionPose = getGoodLimeLightPose();
             if (lastODPose == null) {
                 lastODPose = odPose;
@@ -57,32 +61,39 @@ public class PositionProvider {
 
             if (visionPose != null) {
                 // If robot has not moved much
-                if (Math.abs(lastODPose.getX(DistanceUnit.CM) - odPose.getX(DistanceUnit.CM)) < 0.1 && Math.abs(lastODPose.getY(DistanceUnit.CM) - odPose.getY(DistanceUnit.CM)) < 0.1) {
+                if (Math.abs(startODPose.getX(DistanceUnit.CM) - odPose.getX(DistanceUnit.CM)) < 2 && Math.abs(startODPose.getY(DistanceUnit.CM) - odPose.getY(DistanceUnit.CM)) < 2 && Math.abs(startODPose.getHeading(AngleUnit.DEGREES) - odPose.getHeading(AngleUnit.DEGREES)) < 4 && robotContainer.gamepadEx1.dpadDown.isHeld()) {
 //                if (robotContainer.gamepadEx1.rightStickX() == 0 && robotContainer.gamepadEx1.leftStickX() == 0 && robotContainer.gamepadEx1.leftStickY() == 0) {
                     visionPoseList.add(visionPose);
                 } else {
+                    visionTimer.reset();
                     visionPoseList = new ArrayList<>();
+                    startODPose = odPose;
                 }
             } else {
                 visionTimer.reset();
                 visionPoseList = new ArrayList<>();
+                startODPose = odPose;
             }
             // Add something about rejecting poses that are far from the current average
             if (visionTimer.seconds() > 1 && visionPoseList.size() > 20) {
                 // Average all vision estimates over the time.
                 double llX = 0;
                 double llY = 0;
+                double llH = 0;
                 for (int i = 0; i < visionPoseList.size(); i++) {
                     llX += visionPoseList.get(i).getX(DistanceUnit.CM);
                     llY += visionPoseList.get(i).getY(DistanceUnit.CM);
+                    llH += visionPoseList.get(i).getHeading(AngleUnit.DEGREES);
                 }
 
                 llX = llX / visionPoseList.size();
                 llY = llY / visionPoseList.size();
+                llH = llH / visionPoseList.size();
 
-                visionOffsetPose = new Pose2D(DistanceUnit.CM, llX - odPose.getX(DistanceUnit.CM), llY - odPose.getY(DistanceUnit.CM), AngleUnit.DEGREES, 0);
+                visionOffsetPose = new Pose2D(DistanceUnit.CM, llX - odPose.getX(DistanceUnit.CM), llY - odPose.getY(DistanceUnit.CM), AngleUnit.DEGREES, HelperFunctions.normalizeAngle(llH - odPose.getHeading(AngleUnit.DEGREES)));
                 visionTimer.reset();
                 visionPoseList = new ArrayList<>();
+                startODPose = odPose;
             }
             Status.currentPose = getRobotPose();
             lastODPose = odPose;
