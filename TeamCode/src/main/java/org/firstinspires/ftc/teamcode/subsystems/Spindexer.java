c140f52e9fd3958abe774281a8c078476f70da81
skyhirsch <skyhirsch17@gmail.com>
2026-01-14 22:08:49 -0800

    Update Spindexer.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Spindexer.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Spindexer.java
@@ -58,9 +58,8 @@ public class Spindexer {
 
         double servoPower = calculate();
         if (Math.abs(spindexerError) > 5 && !this.pause) {
-            if (clockwise && spindexerError < -20) {
+            if (spindexerError < 0) {
                 spindexerServo.setPower(-Math.abs(servoPower));
-                // spindexerServo.setPower(Math.abs(servoPower));
             } else {
                 spindexerServo.setPower(servoPower);
             }
